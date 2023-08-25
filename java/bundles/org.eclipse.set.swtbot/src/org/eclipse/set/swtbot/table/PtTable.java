ec16970c29b0a57924a6044a992bc427ea034fbf
TruongQuangSB <131350493+TruongQuangSB@users.noreply.github.com>
2023-08-25 09:02:24 +0200

    Refactoring SWTBot (#11)
    * Refactoring SWTBot
    
    * Apply Suggestion

---
+++ java/bundles/org.eclipse.set.swtbot/src/org/eclipse/set/swtbot/table/PtTable.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2023 DB Netz AG and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+package org.eclipse.set.swtbot.table;
+
+import java.util.List;
+
+/**
+ * @author truong
+ * @param shortcut
+ *            table shortcut
+ * @param tableName
+ *            the table name
+ * @param fixedColumns
+ *            index of fixed column
+ *
+ */
+public record PtTable(String shortcut, String tableName,
+		List<Integer> fixedColumns) {
+	/**
+	 * List table to test
+	 */
+	@SuppressWarnings("boxing")
+	public static List<PtTable> tablesToTest = List.of(
+			new PtTable("ssbb", "Ssbb (Bedieneinrichtungstabelle BÜ)",
+					List.of(0)),
+			new PtTable("ssit", "Ssit (Bedieneinrichtungstabelle Stw)",
+					List.of(0)),
+			new PtTable("sska", "Sska (Elementansteuertabelle)", List.of(0)),
+			new PtTable("sskf", "Sskf (Freimeldetabelle)", List.of(0)),
+			new PtTable("sskg", "Sskg (Gleisschaltmitteltabelle)", List.of(0)),
+			new PtTable("ssko", "Ssko (Schlosstabelle)", List.of(0)),
+			new PtTable("ssks", "Ssks (Signaltabelle)", List.of(0)),
+			new PtTable("sskt",
+					"Sskt (Tabelle der Technik- und Bedienstandorte)",
+					List.of(0)),
+			new PtTable("sskw", "Sskw (Weichentabelle)", List.of(0)),
+			new PtTable("ssla",
+					"Ssla (Tabelle der aneinandergereihten Fahrstraßen)",
+					List.of(0)),
+			new PtTable("sslb", "Sslb (Streckenblocktabelle)", List.of(0)),
+			new PtTable("ssld", "Ssld (Durchrutschweg- und Gefahrpunkttabelle)",
+					List.of(0, 1, 2)),
+			new PtTable("sslf", "Sslf (Flankenschutztabelle)", List.of(0, 1)),
+			new PtTable("ssli", "Ssli (Inselgleistabelle)", List.of(0)),
+			new PtTable("ssln", "Ssln (Nahbedientabelle)", List.of(0)),
+			new PtTable("sslr", "Sslr (Rangierstraßentabelle)", List.of(0)),
+			new PtTable("sslw", "Sslw (Zwieschutzweichentabelle)", List.of(0)),
+			new PtTable("sslz", "Sslz (Zugstraßentabelle)", List.of(0)),
+			new PtTable("ssvu", "Ssvu (Übertragungswegtabelle)",
+					List.of(0, 1)));
+}
