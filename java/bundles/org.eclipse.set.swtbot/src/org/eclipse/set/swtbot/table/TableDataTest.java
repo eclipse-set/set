a9517db645eaa1fa1f2bd2b31c7ae2f0e9380c93
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2023-09-20 14:12:50 +0200

    fix swtbot not check total table

--- java/bundles/org.eclipse.set.swtbot/src/org/eclipse/set/swtbot/table/TableDataTest.java
+++ java/bundles/org.eclipse.set.swtbot/src/org/eclipse/set/swtbot/table/TableDataTest.java
@@ -10,6 +10,7 @@ package org.eclipse.set.swtbot.table;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.IOException;
@@ -17,6 +18,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.commons.csv.CSVRecord;
+import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.nebula.widgets.nattable.grid.layer.GridLayer;
+import org.eclipse.nebula.widgets.nattable.layer.ILayer;
+import org.eclipse.set.utils.table.BodyLayerStack;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -28,11 +33,49 @@ import org.junit.jupiter.params.provider.MethodSource;
  */
 public class TableDataTest extends AbstractTableTest {
 
+	private static final String CELL_VALUE_REPLACE_REGEX = "[\\n\\r]";
+	private int fixedColumnCount;
+
+	private GridLayer gridLayer;
 	List<CSVRecord> referenceData = new LinkedList<>();
+
+	private void compareValue(final ILayer nattableLayer, final int startRow,
+			final int endRow) {
+
+		for (int rowIndex = 0; rowIndex < endRow; rowIndex++) {
+			for (int columnIndex = 0; columnIndex < nattableLayer
+					.getPreferredColumnCount()
+					- fixedColumnCount; columnIndex++) {
+				try {
+					final String cellValue = nattableLayer
+							.getDataValueByPosition(columnIndex, rowIndex)
+							.toString()
+							.replaceAll(CELL_VALUE_REPLACE_REGEX, "");
+					final String referenceValue = referenceData
+							.get(rowIndex + startRow).get(columnIndex + 1)
+							.replaceAll(CELL_VALUE_REPLACE_REGEX, "");
+					assertEquals(referenceValue, cellValue);
+				} catch (final Exception e) {
+					System.out.println(e);
+				}
+
+			}
+		}
+	}
+
+	private void givenFixedColumnCount(final PtTable table) {
+		fixedColumnCount = table.fixedColumns().size();
+	}
+
+	private void givenNattableGridLayer() {
+		final NatTable natTable = nattableBot.widget;
+		final ILayer layer = natTable.getLayer();
+		assertInstanceOf(GridLayer.class, layer);
+		gridLayer = (GridLayer) layer;
+	}
 
 	protected void givenReferenceCSV(final PtTable table) throws IOException {
 		referenceData = loadReferenceFile(table.shortcut());
-
 	}
 
 	/**
@@ -45,27 +88,27 @@ public class TableDataTest extends AbstractTableTest {
 	protected void testTableData(final PtTable table) throws Exception {
 		givenNattableBot(table);
 		givenReferenceCSV(table);
+		givenFixedColumnCount(table);
+		givenNattableGridLayer();
 		whenExistReferenceCSV();
+		thenPtTableColumnHeaderEqualReferenceCSV();
 		thenPtTableDataEqualReferenceCSV();
 	}
 
-	protected void thenPtTableDataEqualReferenceCSV() {
-		for (int rowIndex = 0; rowIndex < nattableBot.rowCount(); rowIndex++) {
-			for (int columnIndex = 0; columnIndex < nattableBot
-					.columnCount(); columnIndex++) {
-				// Skip first cell
-				if (rowIndex == 0 && columnIndex == 0) {
-					continue;
-				}
-				final String cellValue = nattableBot
-						.getCellDataValueByPosition(rowIndex, columnIndex)
-						.replaceAll("[\\n\\r]", "");
-				final String referenceValue = referenceData.get(rowIndex)
-						.get(columnIndex).replaceAll("[\\n\\r]", "");
+	protected void thenPtTableColumnHeaderEqualReferenceCSV() {
+		final int rowCount = gridLayer.getColumnHeaderLayer().getRowCount();
+		compareValue(gridLayer.getColumnHeaderLayer(), 0, rowCount);
 
-				assertEquals(referenceValue, cellValue);
-			}
-		}
+	}
+
+	protected void thenPtTableDataEqualReferenceCSV() {
+
+		assertInstanceOf(BodyLayerStack.class, gridLayer.getBodyLayer());
+		final BodyLayerStack bodyLayerStack = (BodyLayerStack) gridLayer
+				.getBodyLayer();
+		final int startRow = gridLayer.getColumnHeaderLayer().getRowCount();
+		compareValue(bodyLayerStack.getSelectionLayer(), startRow,
+				bodyLayerStack.getSelectionLayer().getPreferredRowCount());
 	}
 
 	protected void whenExistReferenceCSV() {
