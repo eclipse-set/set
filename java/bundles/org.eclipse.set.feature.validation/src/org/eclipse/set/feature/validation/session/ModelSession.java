b36a7225c54237e45f23952122d9a7d49f6ef7a1
Pascal Stücker <131346899+pstuecker@users.noreply.github.com>
2023-10-16 18:17:52 +0200

    Update java/bundles/org.eclipse.set.feature.validation/src/org/eclipse/set/feature/validation/session/ModelSession.java
    Co-authored-by: TruongQuangSB <131350493+TruongQuangSB@users.noreply.github.com>

--- java/bundles/org.eclipse.set.feature.validation/src/org/eclipse/set/feature/validation/session/ModelSession.java
+++ java/bundles/org.eclipse.set.feature.validation/src/org/eclipse/set/feature/validation/session/ModelSession.java
@@ -16,6 +16,7 @@ import java.util.Collections;
 import java.util.EventObject;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
