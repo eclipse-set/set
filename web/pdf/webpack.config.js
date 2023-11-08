d26d12c2a08b6b9338beb9c45f51e80a1a1ab905
Pascal Stücker <stuecker.pascal@scheidt-bachmann-st.de>
2023-11-08 12:46:56 +0100

    Update webpack.config.js to use ES Modules for pdfjs

--- web/pdf/webpack.config.js
+++ web/pdf/webpack.config.js
@@ -1,61 +1,73 @@
-const path = require('path');
-const packageJSON = require('./package.json')
-const fs = require('fs').promises
-const existsSync = require('fs').existsSync
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin');
-const StreamZip = require('node-stream-zip');
+const path = require("path");
+const packageJSON = require("./package.json");
+const fs = require("fs").promises;
+const existsSync = require("fs").existsSync;
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const StreamZip = require("node-stream-zip");
 
 class ExtractPDFJSPlugin {
   async extractPdfJS() {
-    if (!existsSync('pdfjs.zip'))
-      return
+    if (!existsSync("pdfjs.zip")) return;
 
-    await fs.mkdir('pdfjs', { recursive: true })
-    const zip = new StreamZip.async({ file: './pdfjs.zip' });
-    await zip.extract(null, './pdfjs')
-    await zip.close()
+    await fs.mkdir("pdfjs", { recursive: true });
+    const zip = new StreamZip.async({ file: "./pdfjs.zip" });
+    await zip.extract(null, "./pdfjs");
+    await zip.close();
   }
 
   apply(compiler) {
-    compiler.hooks.beforeCompile.tapPromise('ExtractPDFJSPlugin', () => {
-      return this.extractPdfJS()
-    })
+    compiler.hooks.beforeCompile.tapPromise("ExtractPDFJSPlugin", () => {
+      return this.extractPdfJS();
+    });
   }
 }
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   context: __dirname,
   // Disable performance hints (this is a locally served web application)
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
-    maxAssetSize: 512000
+    maxAssetSize: 512000,
   },
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin({ extractComments: false })],
   },
   entry: {
-    index: './index.js',
-    'pdf.worker': 'pdfjs-dist/build/pdf.worker.entry',
+    index: "./index.js",
+    "pdf.worker": "pdfjs-dist/build/pdf.worker.mjs",
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
   },
   plugins: [
     new ExtractPDFJSPlugin(),
     new CopyWebpackPlugin({
       patterns: [
-        'pdfjs/web/viewer.css',
-        'pdfjs/build/pdf.js',
-        { from: 'pdfjs/web/viewer.html', transform: content => content.toString().replace('../build/pdf.js', 'pdf.js').replace('</head>', '<script src="index.js"></script></head>') },
-        { from: 'pdfjs/web/viewer.js', transform: content => content.toString().replace('../build/pdf.worker.js', 'pdf.worker.js') },
-        { from: 'pdfjs/web/images', to: 'images' },
-        { from: 'pdfjs/web/locale', to: 'locale' }
-      ]
-    })
+        "pdfjs/web/viewer.css",
+        "pdfjs/build/pdf.js",
+        {
+          from: "pdfjs/web/viewer.html",
+          transform: (content) =>
+            content
+              .toString()
+              .replace("../build/pdf.js", "pdf.js")
+              .replace("</head>", '<script src="index.js"></script></head>'),
+        },
+        {
+          from: "pdfjs/web/viewer.js",
+          transform: (content) =>
+            content
+              .toString()
+              .replace("../build/pdf.worker.js", "pdf.worker.js"),
+        },
+        { from: "pdfjs/web/images", to: "images" },
+        { from: "pdfjs/web/locale", to: "locale" },
+      ],
+    }),
   ],
 };
