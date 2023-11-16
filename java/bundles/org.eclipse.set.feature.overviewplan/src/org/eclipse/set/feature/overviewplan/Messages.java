11ce5204e5247bb7e5c105f49a65488098c46e37
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2023-11-16 13:50:56 +0100

    implement overviewplan

---
+++ java/bundles/org.eclipse.set.feature.overviewplan/src/org/eclipse/set/feature/overviewplan/Messages.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2023 DB Netz AG and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+package org.eclipse.set.feature.overviewplan;
+
+/**
+ * 
+ */
+public class Messages {
+	/**
+	 * Fehler beim Start des Webservers
+	 */
+	public String WebOverviewPart_WebserverStartErrorMessage;
+
+	/**
+	 * Fehler beim Stopp des Webservers
+	 */
+	public String WebOverviewPart_WebserverStopErrorMessage;
+
+	/**
+	 * Übersichtplan (Web-Ansicht)
+	 */
+	public String WebOverviewplanDescriptionService_ViewName;
+}
