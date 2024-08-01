85e75f4271b65b8c7e408750b8d389a5c3845b08
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2024-08-01 11:36:03 +0200

    Fix exception by open old Model version file

--- java/bundles/org.eclipse.set.core/src/org/eclipse/set/core/fileservice/PlainToolboxFile.java
+++ java/bundles/org.eclipse.set.core/src/org/eclipse/set/core/fileservice/PlainToolboxFile.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -206,7 +207,9 @@ public class PlainToolboxFile extends AbstractToolboxFile {
 	@Override
 	public PlanProFileResource getPlanProResource() {
 		// Plain file contains only technical resource
-		return resources.values().iterator().next();
+		final Iterator<PlanProFileResource> resourcesIte = resources.values()
+				.iterator();
+		return resourcesIte.hasNext() ? resourcesIte.next() : null;
 	}
 
 	@Override
