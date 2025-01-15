33d94b146d00c5dc734c5bb34012235f545b91dd
Marius Heine <m.heine@geprog.com>
2025-01-15 09:48:41 +0100

    revert formatting

--- java/bundles/org.eclipse.set.feature.table.pt1/src/org/eclipse/set/feature/table/pt1/sskz/SskzTransformator.java
+++ java/bundles/org.eclipse.set.feature.table.pt1/src/org/eclipse/set/feature/table/pt1/sskz/SskzTransformator.java
@@ -227,16 +227,17 @@ public class SskzTransformator extends AbstractPlanPro2TableModelTransformator {
 					.map(ele -> switch (ele) {
 					case final Signal signal -> signal.getBezeichnung()
 							.getBezeichnungTabelle().getWert();
-									case final W_Kr_Gsp_Element gsp -> gsp.getBezeichnung()
-													.getBezeichnungTabelle().getWert();
+							case final W_Kr_Gsp_Element gsp -> gsp
+									.getBezeichnung().getBezeichnungTabelle()
+									.getWert();
 					default -> "";
 
 					}).toList();
 			yield String.format("%s (%s)", translate(pzbArt),
 					String.join(",", pzbElementBezugspunkt));
 		}
-				case final Schluesselsperre schluessel -> schluessel.getBezeichnung()
-						.getBezeichnungTabelle().getWert();
+			case final Schluesselsperre schluessel -> schluessel
+					.getBezeichnung().getBezeichnungTabelle().getWert();
 		default -> null;
 		};
 		return getNullableObject(object, destignationFunc::apply).orElse("");
