06277ef8e914b8bbb84a481318443bcedfdf97ab
Marius Heine <m.heine@geprog.com>
2025-02-24 01:06:54 +0100

    use curl for better cross compatibility

--- releng/eclipse/prepare-eclipse.sh
+++ releng/eclipse/prepare-eclipse.sh
@@ -25,7 +25,7 @@ if [ ! -d "$ECLIPSE_HOME" ]; then
   if [[ "$OSTYPE" == "linux-gnu" ]]; then
     echo "Downloading eclipse for Linux..."
     mkdir ./tmp-eclipse-download
-    wget -O ./tmp-eclipse-download/eclipse.tar.gz https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-linux-gtk-x86_64.tar.gz
+    curl -o ./tmp-eclipse-download/eclipse.tar.gz https://archive.eclipse.org/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-linux-gtk-x86_64.tar.gz
     tar -xzf ./tmp-eclipse-download/eclipse.tar.gz --directory ./tmp-eclipse-download --warning=no-unknown-keyword
     sleep 5s
     mkdir -p $ECLIPSE_HOME
@@ -36,7 +36,7 @@ if [ ! -d "$ECLIPSE_HOME" ]; then
   # Check for macOS (darwin is the OS type identifier used by macOS)
   if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Downloading eclipse for macOS..."
-    wget -O ./eclipse.dmg https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-macosx-cocoa-x86_64.dmg
+    curl -o ./eclipse.dmg https://archive.eclipse.org/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-macosx-cocoa-x86_64.dmg
     echo "TODO: How to install eclipse under macos... Please install or unpack the downloaded eclipse.dmg by yourself to $ECLIPSE_HOME and restart the script."
     exit 1
   fi
@@ -45,7 +45,7 @@ if [ ! -d "$ECLIPSE_HOME" ]; then
   if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
     echo "Downloading eclipse for Windows..."
     mkdir ./tmp-eclipse-download
-    wget -O ./tmp-eclipse-download/eclipse.zip https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-win32-x86_64.zip
+    curl -o ./tmp-eclipse-download/eclipse.zip https://archive.eclipse.org/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-win32-x86_64.zip
     unzip ./tmp-eclipse-download/eclipse.zip -d ./tmp-eclipse-download
     mkdir -p $ECLIPSE_HOME
     mv ./tmp-eclipse-download/eclipse/* $ECLIPSE_HOME
