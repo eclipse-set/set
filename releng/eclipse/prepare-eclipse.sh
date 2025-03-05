2a9564dff84981c2f19da4c6a07cad4d4307ccfe
Marius Heine <m.heine@geprog.com>
2025-03-05 14:10:57 +0100

    handle existing eclipse homes better

--- releng/eclipse/prepare-eclipse.sh
+++ releng/eclipse/prepare-eclipse.sh
@@ -9,24 +9,40 @@
 #
 # Purpose:
 # Downloads eclipse ide with correct version and prepares it for development
+finish() {
+  read -p "Script finished. Press any key to proceed..." x
+  exit $1
+}
+
 if [ -z "$ECLIPSE_HOME" ]; then
   echo "Error: ECLIPSE_HOME not found"
   exit 1
 fi
 
+askForInstall=""
 if [ ! -d "$ECLIPSE_HOME" ]; then
-  read -p "Your eclipse home is not existing. Do you want to download eclipse? (y or n) " install
+  askForInstall="Your eclipse home is not existing."
+elif [ ! $(ls -A "$ECLIPSE_HOME" 2>/dev/null) ]; then
+  askForInstall="Your eclipse home is empty."
+elif [ ! -f "$ECLIPSE_HOME/eclipse" ]; then
+  echo "Error: Your eclipse home is existing but does not contain eclipse. Please provide another eclipse home"
+  finish 1
+fi
+
+if [ ! -z "$askForInstall" ]; then
+  echo "$askForInstall"
+  read -p "Do you want to install eclipse there? (y or n)" install
 
   if [ "$install" != "y" ]; then
     echo "Error: ECLIPSE_HOME invalid"
-    exit 1
+    finish 1
   fi
 
   if [[ "$OSTYPE" == "linux-gnu" ]]; then
     echo "Downloading eclipse for Linux..."
     mkdir ./tmp-eclipse-download
     curl -o ./tmp-eclipse-download/eclipse.tar.gz https://archive.eclipse.org/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-linux-gtk-x86_64.tar.gz
-    tar -xzf ./tmp-eclipse-download/eclipse.tar.gz --directory ./tmp-eclipse-download --warning=no-unknown-keyword
+    tar --quiet -xzf ./tmp-eclipse-download/eclipse.tar.gz --directory ./tmp-eclipse-download --warning=no-unknown-keyword
     sleep 5s
     mkdir -p $ECLIPSE_HOME
     mv ./tmp-eclipse-download/eclipse/* $ECLIPSE_HOME
@@ -38,7 +54,7 @@ if [ ! -d "$ECLIPSE_HOME" ]; then
     echo "Downloading eclipse for macOS..."
     curl -o ./eclipse.dmg https://archive.eclipse.org/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-macosx-cocoa-x86_64.dmg
     echo "TODO: How to install eclipse under macos... Please install or unpack the downloaded eclipse.dmg by yourself to $ECLIPSE_HOME and restart the script."
-    exit 1
+    finish 1
   fi
 
   # Check for Windows (Windows Subsystem for Linux, or WSL, uses a linux-gnu like OS type)
@@ -46,7 +62,7 @@ if [ ! -d "$ECLIPSE_HOME" ]; then
     echo "Downloading eclipse for Windows..."
     mkdir ./tmp-eclipse-download
     curl -o ./tmp-eclipse-download/eclipse.zip https://archive.eclipse.org/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-win32-x86_64.zip
-    unzip ./tmp-eclipse-download/eclipse.zip -d ./tmp-eclipse-download
+    unzip -q ./tmp-eclipse-download/eclipse.zip -d ./tmp-eclipse-download
     mkdir -p $ECLIPSE_HOME
     mv ./tmp-eclipse-download/eclipse/* $ECLIPSE_HOME
     rm -R ./tmp-eclipse-download
@@ -59,7 +75,7 @@ if [ $eclipseBuildId != "eclipse.buildId=4.32.0.20240606-1231" ]; then
   echo "You can rerun this script with an nonexisting ECLIPSE_HOME and this script will download the correct version of eclipse for you."
   read -p "Do you want to continue with the current eclipse? (y or n) " continue
   if [ "$continue" != "y" ]; then
-    exit 1
+    finish 1
   fi 
 fi
 
@@ -69,3 +85,5 @@ echo "Install eclipse plugins now..."
 
 echo "Prepare development now..."
 node ./development_prepare.js
+
+finish 0
