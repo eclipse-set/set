a4465e10a0df2739c32c264f3e3edf6992023ebf
TruongQuangSB <131350493+TruongQuangSB@users.noreply.github.com>
2025-03-07 09:56:14 +0100

    Update Build-Instruction and remove redundant bundle (#1345)
    * Update Build-Instruction and remove redundant bundle
    
    * Update pom.xml
    
    * update
    
    * add script for compile web package and install ide plugins
    
    * Update development_prepare.js
    
    * Update vite.config.js
    
    * Update BUILD_INSTRUCTIONS.md
    
    Co-authored-by: Marius Heine <m.heine@geprog.com>
    
    * Add maven setting guide
    
    * Add Eclipse IDE plugin
    
    * Update install_eclipse_plugin.sh
    
    * Update install_eclipse_plugin.sh
    
    * make prepare eclipse script executable
    
    * check for eclipse executable and hide debug statements
    
    * fix  lock file
    
    * add hugo build locks to gitignore
    
    * create .env files for web packages
    
    * use curl for better cross compatibility
    
    * add sleep for unzip
    
    * updated build instructions
    
    * fix script under windows
    
    * handle existing eclipse homes better
    
    * fix tar command
    
    * do not unzip pdfjs because this is done by build script
    
    * fix syntax error in if statement
    
    * properly create .env file in all environments
    
    * add .env file to gitignore
    
    * complete maven settings explanation
    
    * fix text and pdf viewer build in CI
    
    ---------
    
    Co-authored-by: Marius Heine <m.heine@geprog.com>

---
+++ releng/eclipse/development_prepare.js
@@ -0,0 +1,91 @@
+const exec = require('child_process').exec
+const original_path = __dirname
+const https = require('https')
+const fs = require('fs')
+const pdfjsVersion = require("../../web/pdf/package.json").dependencies["pdfjs-dist"].substring(1)
+const eclipseHome = process.env.ECLIPSE_HOME
+fs.writeFileSync('.env', `ECLIPSE_HOME=${eclipseHome}`);
+const envFileSymlinkCommand = 'ln ../../releng/eclipse/.env .env';
+
+// Build and copy web package to EclipseDirectory
+copyResourceDirectory()
+compileWebPackages()
+
+async function copyResourceDirectory() {
+  console.log("****Compile Textviewer****")
+  const copyFunc = (src, dest) => {
+    console.log(`Copy ${src} to ${dest}`)
+    fs.cpSync(src, dest, { recursive: true, force: true })
+  }
+  const featureBundlePath = "../../java/bundles/org.eclipse.set.feature/rootdir"
+  copyFunc(featureBundlePath, eclipseHome)
+  console.log("****End****")
+}
+
+async function compileWebPackages() {
+  // News
+  console.log("****Compile News****")
+  await doCommand(`cd ../../web/news && hugo -d ${eclipseHome}/web/news`)
+
+  // Developerhelp
+  console.log("****Compile Developerhelp****")
+  await doCommand(`cd ../../web/developerhelp && hugo -d ${eclipseHome}/web/developerhelp`)
+
+  // About
+  console.log("****Compile About****")
+  await doCommand(`cd ../../web/about && hugo -d ${eclipseHome}/web/about`)
+  // Textviewer
+  console.log("****Compile Textviewer****")
+  await doCommand(`cd ../../web/textviewer && ${envFileSymlinkCommand} && npm ci && npm run build`)
+
+  // Siteplan
+  console.log("****Compile Siteplan****")
+  await doCommand(`cd ../../web/siteplan && ${envFileSymlinkCommand} && npm ci && npm run build`)
+
+  // Pdfjs
+  console.log("****Compile Pdfjs****")
+  compilePdfJs(
+    `https://github.com/mozilla/pdf.js/releases/download/v${pdfjsVersion}/pdfjs-${pdfjsVersion}-dist.zip`, "../../web/pdf/pdfjs.zip")
+}
+
+async function compilePdfJs(fileUrl, outputPath) {
+  await new Promise(resolve => {
+    https.get(fileUrl, response => {
+      if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location) {
+        console.log("Redirecting URL nach: " + response.headers.location)
+        return compilePdfJs(response.headers.location, outputPath)
+      }
+
+      if (response.statusCode !== 200) {
+        console.error("Error: HTTP " + response.statusCode)
+      }
+      const fileStream = fs.createWriteStream(outputPath)
+      response.pipe(fileStream)
+      fileStream.on("finish", () => {
+        console.log("Download complete")
+        fileStream.close()
+      }).on("error", err => {
+        console.log("Download Error: " + err.message)
+      })
+      resolve(null)
+    })
+  })
+  await doCommand(`cd ../../web/pdf && ${envFileSymlinkCommand} && npm ci && npm run build`)
+}
+
+async function doCommand(command) {
+  await new Promise(resolve => {
+    exec(`${command}`, (error, stderr, stdout) => {
+      if (error) {
+        console.error(`Error executing command: ${error.message}`);
+      } else if (stderr) {
+        console.error(`Error output: ${stderr}`);
+      } else {
+        console.log(`Command Output: ${stdout}`);
+      }
+
+      console.log("****End****")
+      resolve(null)
+    })
+  })
+}
---
+++ releng/eclipse/prepare-eclipse.sh
@@ -0,0 +1,89 @@
+#! /bin/bash
+#
+# Copyright (c) 2025 Scheidt & Bachmann System Technik GmbH.
+# All rights reserved.
+#
+# @author Heine
+#
+# Synopsis: prepare eclipse ide
+#
+# Purpose:
+# Downloads eclipse ide with correct version and prepares it for development
+finish() {
+  read -p "Script finished. Press any key to proceed..." x
+  exit $1
+}
+
+if [ -z "$ECLIPSE_HOME" ]; then
+  echo "Error: ECLIPSE_HOME not found"
+  exit 1
+fi
+
+askForInstall=""
+if [ ! -d "$ECLIPSE_HOME" ]; then
+  askForInstall="Your eclipse home is not existing."
+elif [ ! "$(ls -A "$ECLIPSE_HOME" 2>/dev/null)" ]; then
+  askForInstall="Your eclipse home is empty."
+elif [ ! -f "$ECLIPSE_HOME/eclipse" ]; then
+  echo "Error: Your eclipse home is existing but does not contain eclipse. Please provide another eclipse home"
+  finish 1
+fi
+
+if [ ! -z "$askForInstall" ]; then
+  echo "$askForInstall"
+  read -p "Do you want to install eclipse there? (y or n)" install
+
+  if [ "$install" != "y" ]; then
+    echo "Error: ECLIPSE_HOME invalid"
+    finish 1
+  fi
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo "Downloading eclipse for Linux..."
+    mkdir ./tmp-eclipse-download
+    curl -o ./tmp-eclipse-download/eclipse.tar.gz https://archive.eclipse.org/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-linux-gtk-x86_64.tar.gz
+    tar -xzf ./tmp-eclipse-download/eclipse.tar.gz --directory ./tmp-eclipse-download --warning=no-unknown-keyword
+    sleep 5s
+    mkdir -p $ECLIPSE_HOME
+    mv ./tmp-eclipse-download/eclipse/* $ECLIPSE_HOME
+    rm -R ./tmp-eclipse-download
+  fi
+
+  # Check for macOS (darwin is the OS type identifier used by macOS)
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Downloading eclipse for macOS..."
+    curl -o ./eclipse.dmg https://archive.eclipse.org/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-macosx-cocoa-x86_64.dmg
+    echo "TODO: How to install eclipse under macos... Please install or unpack the downloaded eclipse.dmg by yourself to $ECLIPSE_HOME and restart the script."
+    finish 1
+  fi
+
+  # Check for Windows (Windows Subsystem for Linux, or WSL, uses a linux-gnu like OS type)
+  if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
+    echo "Downloading eclipse for Windows..."
+    mkdir ./tmp-eclipse-download
+    curl -o ./tmp-eclipse-download/eclipse.zip https://archive.eclipse.org/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-win32-x86_64.zip
+    unzip -q ./tmp-eclipse-download/eclipse.zip -d ./tmp-eclipse-download
+    mkdir -p $ECLIPSE_HOME
+    mv ./tmp-eclipse-download/eclipse/* $ECLIPSE_HOME
+    rm -R ./tmp-eclipse-download
+  fi
+fi
+
+eclipseBuildId=$(cat $ECLIPSE_HOME/configuration/config.ini | grep eclipse.buildId)
+if [ $eclipseBuildId != "eclipse.buildId=4.32.0.20240606-1231" ]; then
+  echo "You are using a different version than the preferred version 2024-06 (4.32) of eclipse: ${eclipseBuildId}."
+  echo "You can rerun this script with an nonexisting ECLIPSE_HOME and this script will download the correct version of eclipse for you."
+  read -p "Do you want to continue with the current eclipse? (y or n) " continue
+  if [ "$continue" != "y" ]; then
+    finish 1
+  fi 
+fi
+
+cd $(dirname "$0")
+echo "Install eclipse plugins now..."
+./install_eclipse_plugin.sh
+
+echo "Prepare development now..."
+node ./development_prepare.js
+
+finish 0
