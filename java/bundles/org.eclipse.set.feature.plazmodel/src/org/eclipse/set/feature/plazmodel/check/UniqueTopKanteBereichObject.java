5f69b9ffd8df918a2b10c586a9bf13f7501b43a0
TruongQuangSB <131350493+TruongQuangSB@users.noreply.github.com>
2025-03-24 13:57:13 +0100

    PlaZ-Model: add check for uniqueness of TOP_Kante in Bereichobjekt (#1474)

---
+++ java/bundles/org.eclipse.set.feature.plazmodel/src/org/eclipse/set/feature/plazmodel/check/UniqueTopKanteBereichObject.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2025 DB InfraGO AG and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ */
+package org.eclipse.set.feature.plazmodel.check;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.eclipse.set.model.planpro.Basisobjekte.Bereich_Objekt;
+import org.eclipse.set.model.planpro.Basisobjekte.Bereich_Objekt_Teilbereich_AttributeGroup;
+import org.eclipse.set.model.planpro.Geodaten.TOP_Kante;
+import org.eclipse.set.model.plazmodel.PlazError;
+import org.eclipse.set.model.plazmodel.PlazFactory;
+import org.eclipse.set.model.validationreport.ValidationSeverity;
+import org.eclipse.set.ppmodel.extensions.container.MultiContainer_AttributeGroup;
+import org.osgi.service.component.annotations.Component;
+
+import com.google.common.collect.Streams;
+
+/**
+ * The {@link TOP_Kante} of the
+ * {@link Bereich_Objekt_Teilbereich_AttributeGroup} in a
+ * {@link Bereich_Objekt}} should be unique
+ * 
+ * @author truong
+ */
+@Component
+public class UniqueTopKanteBereichObject extends AbstractPlazContainerCheck
+		implements PlazCheck {
+
+	@Override
+	public String checkType() {
+		return "Mehrfache TOP_Kante"; //$NON-NLS-1$
+	}
+
+	@Override
+	public String getDescription() {
+		return "Jede Top_Kante, die Bereichsobjekt defniert, sind einzigartig"; //$NON-NLS-1$
+	}
+
+	@Override
+	public String getGeneralErrMsg() {
+		return "Für das Bereichsobjekt {Objektart} sind mehrere Teilbereiche auf der TOP-Kante {GUID} definiert."; //$NON-NLS-1$
+	}
+
+	@Override
+	protected List<PlazError> run(
+			final MultiContainer_AttributeGroup container) {
+		final List<PlazError> result = new ArrayList<>();
+		Streams.stream(container.getBereichObjekt()).forEach(bo -> {
+			final List<TOP_Kante> topKanten = bo.getBereichObjektTeilbereich()
+					.stream()
+					.map(botb -> botb.getIDTOPKante().getValue())
+					.toList();
+
+			final Set<TOP_Kante> duplicateTopKanten = topKanten.parallelStream()
+					.filter(tk -> Collections.frequency(topKanten, tk) > 1)
+					.collect(Collectors.toSet());
+			if (duplicateTopKanten.isEmpty()) {
+				return;
+			}
+			duplicateTopKanten.forEach(topKante -> bo
+					.getBereichObjektTeilbereich()
+					.stream()
+					.filter(botb -> botb.getIDTOPKante().getValue() == topKante)
+					.forEach(botb -> result
+							.add(createError(bo, botb, topKante))));
+
+		});
+		return result;
+	}
+
+	private PlazError createError(final Bereich_Objekt bo,
+			final Bereich_Objekt_Teilbereich_AttributeGroup botb,
+			final TOP_Kante topKante) {
+		final PlazError error = PlazFactory.eINSTANCE.createPlazError();
+		error.setSeverity(ValidationSeverity.WARNING);
+		error.setObject(botb);
+		error.setType(checkType());
+		error.setMessage(
+				transformErrorMsg(Map.of("Objektart", bo.eClass().getName(), //$NON-NLS-1$
+						"GUID", topKante.getIdentitaet().getWert()))); //$NON-NLS-1$
+		return error;
+	}
+
+}
