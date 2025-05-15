586b57e7951f38ae1c4ddd1138a9eeec65e3be97
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2025-05-15 14:52:44 +0200

    Add PlazModel Check for overlap part of region

---
+++ java/bundles/org.eclipse.set.feature.plazmodel/src/org/eclipse/set/feature/plazmodel/check/TeilbereichOverlappCheck.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2025 DB InfraGO AG and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ */
+package org.eclipse.set.feature.plazmodel.check;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.eclipse.set.model.planpro.Basisobjekte.Bereich_Objekt;
+import org.eclipse.set.model.planpro.Basisobjekte.Bereich_Objekt_Teilbereich_AttributeGroup;
+import org.eclipse.set.model.plazmodel.PlazError;
+import org.eclipse.set.model.plazmodel.PlazFactory;
+import org.eclipse.set.model.validationreport.ValidationSeverity;
+import org.eclipse.set.ppmodel.extensions.container.MultiContainer_AttributeGroup;
+import org.osgi.service.component.annotations.Component;
+
+import com.google.common.collect.Streams;
+
+/**
+ * 
+ */
+@Component
+public class TeilbereichOverlappCheck extends AbstractPlazContainerCheck
+		implements PlazCheck {
+
+	@Override
+	public String checkType() {
+		return "Überlappen Teilbereiche"; //$NON-NLS-1$
+	}
+
+	@Override
+	public String getDescription() {
+		return "Teilbereichsgrenzen der LST-Objekte sind pausible"; //$NON-NLS-1$
+	}
+
+	@Override
+	public String getGeneralErrMsg() {
+		return "Es gibt Teilbereiche, die sich überlappen, in Objekt {GUID}"; //$NON-NLS-1$
+	}
+
+	@Override
+	protected List<PlazError> run(
+			final MultiContainer_AttributeGroup container) {
+		return Streams.stream(container.getBereichObjekt()).flatMap(bo -> {
+			final List<List<Bereich_Objekt_Teilbereich_AttributeGroup>> sameTopKanteTeilBereich = bo
+					.getBereichObjektTeilbereich()
+					.stream()
+					.collect(Collectors.groupingBy(
+							botb -> botb.getIDTOPKante().getValue()))
+					.values()
+					.stream()
+					.filter(l -> l.size() > 1)
+					.toList();
+			final Set<Bereich_Objekt_Teilbereich_AttributeGroup> overlappBotb = new HashSet<>();
+			sameTopKanteTeilBereich.forEach(botbs -> {
+				for (int i = 0; i < botbs.size(); i++) {
+					for (int j = i + 1; j < botbs.size(); j++) {
+						if (isTeilbereichOverlapp(botbs.get(i), botbs.get(j))) {
+							overlappBotb.add(botbs.get(i));
+							overlappBotb.add(botbs.get(j));
+						}
+					}
+				}
+			});
+			if (!overlappBotb.isEmpty()) {
+				return overlappBotb.stream()
+						.map(botb -> createPlaZError(bo, botb));
+			}
+			return null;
+		}).filter(Objects::nonNull).toList();
+	}
+
+	private static boolean isTeilbereichOverlapp(
+			final Bereich_Objekt_Teilbereich_AttributeGroup botb1,
+			final Bereich_Objekt_Teilbereich_AttributeGroup botb2) {
+		if (botb1.getIDTOPKante().getValue() != botb2.getIDTOPKante()
+				.getValue()) {
+			return false;
+		}
+		final BigDecimal limitA1 = botb1.getBegrenzungA().getWert();
+		final BigDecimal limitB1 = botb1.getBegrenzungB().getWert();
+		final BigDecimal limitA2 = botb2.getBegrenzungA().getWert();
+		final BigDecimal limitB2 = botb2.getBegrenzungB().getWert();
+		return limitA1.compareTo(limitB2) < 0 && limitA1.compareTo(limitA2) > 0
+				|| limitB1.compareTo(limitB2) < 0
+						&& limitB1.compareTo(limitA2) > 0;
+	}
+
+	private PlazError createPlaZError(final Bereich_Objekt bo,
+			final Bereich_Objekt_Teilbereich_AttributeGroup botb) {
+		final PlazError plazError = PlazFactory.eINSTANCE.createPlazError();
+		plazError.setSeverity(ValidationSeverity.ERROR);
+		plazError.setMessage(transformErrorMsg(
+				Map.of("GUID", bo.getIdentitaet().getWert()))); //$NON-NLS-1$
+		plazError.setType(checkType());
+		plazError.setObject(botb);
+		return plazError;
+	}
+
+}
