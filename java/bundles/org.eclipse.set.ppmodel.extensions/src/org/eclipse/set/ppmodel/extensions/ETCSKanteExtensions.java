b9c68801953e1859a6b3704d7d52889c5f3c4624
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2025-06-04 15:26:34 +0200

    Fix duplicate value by column K

--- java/bundles/org.eclipse.set.ppmodel.extensions/src/org/eclipse/set/ppmodel/extensions/ETCSKanteExtensions.java
+++ java/bundles/org.eclipse.set.ppmodel.extensions/src/org/eclipse/set/ppmodel/extensions/ETCSKanteExtensions.java
@@ -11,10 +11,17 @@
 package org.eclipse.set.ppmodel.extensions;
 
 import static org.eclipse.set.ppmodel.extensions.EObjectExtensions.getNullableObject;
+import static org.eclipse.set.ppmodel.extensions.ETCSKnotenExtensions.getKnotenPosition;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
+import org.eclipse.set.basis.graph.TopPath;
+import org.eclipse.set.basis.graph.TopPoint;
+import org.eclipse.set.core.services.Services;
 import org.eclipse.set.model.planpro.Balisentechnik_ETCS.ETCS_Kante;
 import org.eclipse.set.model.planpro.Balisentechnik_ETCS.ETCS_Knoten;
 import org.eclipse.set.model.planpro.Geodaten.TOP_Kante;
@@ -24,7 +31,7 @@ import org.eclipse.set.model.planpro.Geodaten.TOP_Kante;
  * 
  * @author truong
  */
-public class ECTSKanteExtensions extends BasisObjektExtensions {
+public class ETCSKanteExtensions extends BasisObjektExtensions {
 	/**
 	 * @param etcsEdge
 	 *            the {@link ETCS_Kante}
@@ -58,4 +65,35 @@ public class ECTSKanteExtensions extends BasisObjektExtensions {
 				.filter(Objects::nonNull)
 				.toList();
 	}
+
+	/**
+	 * @param etcsEdge
+	 *            the {@link ETCS_Kante}
+	 * @return the path between the both {@link ETCS_Knoten} of this ETCS_Kante
+	 */
+	public static TopPath getETCSKantePath(final ETCS_Kante etcsEdge) {
+		final ETCS_Knoten etcsKnotenA = getETCSKnotenA(etcsEdge);
+		final ETCS_Knoten etcsKnotenB = getETCSKnotenB(etcsEdge);
+		if (etcsKnotenA == null || etcsKnotenB == null) {
+			throw new IllegalArgumentException(
+					String.format("ETCS_Kante %s is missing ETCS_Knoten", //$NON-NLS-1$
+							etcsEdge.getIdentitaet().getWert()));
+		}
+
+		final Map<TOP_Kante, BigDecimal> posA = getKnotenPosition(etcsKnotenA);
+		final Map<TOP_Kante, BigDecimal> posB = getKnotenPosition(etcsKnotenB);
+		return posA.entrySet()
+				.stream()
+				.map(a -> new TopPoint(a.getKey(), a.getValue()))
+				.flatMap(pointA -> posB.entrySet()
+						.stream()
+						.map(b -> new TopPoint(b.getKey(), b.getValue()))
+						.map(pointB -> Services.getTopGraphService()
+								.findShortestPath(pointA, pointB)))
+				.filter(Optional::isPresent)
+				.map(Optional::get)
+				.min((p1, p2) -> p1.length().compareTo(p2.length()))
+				.orElse(null);
+	}
+
 }
