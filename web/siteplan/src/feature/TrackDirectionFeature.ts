21e8c213cb414ed4f8dc43c0438d28ba0a4a5ac6
Jan-Erik Voigt <voigt.jan-erik@scheidt-bachmann-st.de>
2025-07-02 13:02:31 +0200

    successfully show ugly track direction marker

--- web/siteplan/src/feature/TrackDirectionFeature.ts
+++ web/siteplan/src/feature/TrackDirectionFeature.ts
@@ -1,9 +1,9 @@
 
 import LageplanFeature from '@/feature/LageplanFeature'
+import { Coordinate } from '@/model/Position'
 import { SiteplanState } from '@/model/SiteplanModel'
 import { ISvgElement } from '@/model/SvgElement'
 import Track from '@/model/Track'
-import SvgDrawTrackDirectionArrow from '@/util/SVG/Draw/SvgDrawTrackDirectionArrow'
 import { Feature } from 'ol'
 import { getCenter } from 'ol/extent'
 import Geometry from 'ol/geom/Geometry'
@@ -18,32 +18,37 @@ import { createFeature, FeatureType } from './FeatureInfo'
  */
 export default class TrackDirectionFeature extends LageplanFeature<Track> {
   static readonly TDF_FEATURE = (feature,resolution) => {
-    // const baseResolution = this.map.getView().getResolutionForZoom(this.svgService.getBaseZoomLevel())
-    const baseResolution = 0.1 // TODO????
-    const scale = baseResolution / resolution
+    // const baseResolution = this.map.getView()
+    // .getResolutionForZoom(this.svgService.getBaseZoomLevel())
+    // const baseResolution = 0.1 // TODO????
+    // const scale = baseResolution / resolution
+
+    const rotation = feature.values_.data.model.data
 
     const style = new OlStyle({
       image: new OlIcon({
         opacity: 1,
         // src: 'https://openlayers.org/en/latest/examples/data/icon.png',
-        src: 'SvgKatalog/GleisTopologischeAusrichtung.svg',
+        src: 'SvgKatalog/trackdirectionmarker.png',
         rotateWithView: true,
-        scale
-        /* anchor: [0.5,0.5],
+        scale: 0.4,
+        rotation: rotation,
+        anchor: [0.5,0.5],
         anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction' */
+        anchorYUnits: 'fraction'
 
       })
       // geometry: geometry
 
     })
     // console.log('tdf icon size: ',style.getImage.getSize())
-    style.getImage()?.setRotation(((0.0) * Math.PI) / 180) // TODO
+    // style.getImage()?.setRotation(0) // TODO
     return style
   }
 
   getFeatures (model: SiteplanState): Feature<Geometry>[] {
-    const res = this.getObjectsModel(model).map(element => this.createTrackDirectionArrowFeature(element))
+    const res = this.getObjectsModel(model).map(element => this.createTrackDirectionArrowFeatures(element))
+      .flat()
     return res
   }
 
@@ -55,150 +60,93 @@ export default class TrackDirectionFeature extends LageplanFeature<Track> {
     return this.svgService.getFeatureSvg(track, FeatureType.TrackDirectionArrow) // no data!
   }
 
-  private createTrackDirectionArrowFeature (track: Track): Feature<Geometry> {
-    const geometry = new OlPoint([
-      track.sections[0].segments[0].positions[0].x,
-      track.sections[0].segments[0].positions[0].y
-    ])
+  private createTrackDirectionArrowFeatures (track: Track): Feature<Geometry>[] {
+    const markers: Feature<Geometry>[] = []
 
-    const drawer = new SvgDrawTrackDirectionArrow(this.svgService.getCatalogService())
-    // const svg = drawer.drawSVG(track, undefined)
+    function distance_coord (a: Coordinate, b: Coordinate): number {
+      return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
+    }
 
-    const svg = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
-    xmlns:toolbox="tag:scheidt-bachmann-st.com,2021:hs" viewBox="0 0 100 150" stroke-width="2.5" stroke="black">
-    <defs>
-        <g class="BUE" id="Bue0">
-            <rect width="30" height="55" y="12.5" fill="white" />
-            <circle r="9.5" cx="15" cy="29" />
-            <circle r="9.5" cx="15" cy="52" fill="white" />
-            <toolbox:anchor id="mastAnkerTop" x="15" y="12.5" />
-            <toolbox:anchor id="mastAnkerBottom" x="15" y="65" />
-            <toolbox:rectbound x="0" y="12.5" width="30" height="55" />
-        </g>
-        <g class="BUE" id="Bue2">
-            <rect width="20" height="88" y="12.5" rx="1" />
-            <rect fill="white" x="18" y="4" width="12" height="12" stroke="white" transform="rotate(45)" />
-            <rect fill="white" x="33" y="19" width="12" height="12" stroke="white" transform="rotate(45)" />
-            <rect fill="white" x="48" y="34" width="12" height="12" stroke="white" transform="rotate(45)" />
-            <rect fill="white" x="63" y="49" width="12" height="12" stroke="white" transform="rotate(45)" />
-            <toolbox:anchor id="mastAnkerTop" x="10" y="12.5" />
-            <toolbox:anchor id="mastAnkerBottom" x="10" y="100" />
-            <toolbox:rectbound x="0" y="12.5" width="20" height="88" />
-        </g>
-        <g class="BUE" id="Bue21R">
-            <rect width="20" height="88" y="12.5" rx="1" />
-            <rect fill="white" x="40" y="26" width="12" height="12" stroke="white" transform="rotate(45)" />
-            <toolbox:anchor id="mastAnkerTop" x="10" y="12.5" />
-            <toolbox:anchor id="mastAnkerBottom" x="10" y="100" />
-            <toolbox:rectbound x="0" y="12.5" width="20" height="88" />
-        </g>
-        <g class="BUE" id="Bue22R">
-            <rect width="20" height="88" y="12.5" rx="1" />
-            <rect fill="white" x="31" y="17" width="12" height="12" stroke="white" transform="rotate(45)" />
-            <rect fill="white" x="50" y="36" width="12" height="12" stroke="white" transform="rotate(45)" />
-            <toolbox:anchor id="mastAnkerTop" x="10" y="12.5" />
-            <toolbox:anchor id="mastAnkerBottom" x="10" y="100" />
-            <toolbox:rectbound x="0" y="12.5" width="20" height="88" />
-        </g>
-        <g class="BUE" id="Bue23R">
-            <rect width="20" height="88" y="12.5" rx="1" />
-            <rect fill="white" x="25" y="11" width="12" height="12" stroke="white" transform="rotate(45)" />
-            <rect fill="white" x="42" y="28" width="12" height="12" stroke="white" transform="rotate(45)" />
-            <rect fill="white" x="59" y="45" width="12" height="12" stroke="white" transform="rotate(45)" />
-            <toolbox:anchor id="mastAnkerTop" x="10" y="12.5" />
-            <toolbox:anchor id="mastAnkerBottom" x="10" y="100" />
-            <toolbox:rectbound x="0" y="12.5" width="20" height="88" />
-        </g>
-        <g class="BUE" id="So15">
-            <rect fill="white" width="14" height="98" y="0" />
-            <rect fill="black" x="0" y="14" width="14" height="14" />
-            <rect fill="black" x="0" y="42" width="14" height="14" />
-            <rect fill="black" x="0" y="70" width="14" height="14" />
-            <toolbox:anchor id="mastAnkerTop" x="7" y="0" />
-            <toolbox:anchor id="mastAnkerBottom" x="7" y="98" />
-            <toolbox:rectbound x="0" y="0" width="14" height="98" />
-        </g>
-        <g class="BUE" id="So14">
-            <rect fill="white" width="14" height="98" y="0" />
-            <rect fill="black" x="0" y="14" width="14" height="14" />
-            <rect fill="black" x="0" y="42" width="14" height="14" />
-            <rect fill="black" x="0" y="70" width="14" height="14" />
-            <rect fill="black" x="-5" y="97" width="24" height="1" />
-            <toolbox:anchor id="mastAnkerBottom" x="7" y="98" />
-            <toolbox:rectbound x="0" y="0" width="14" height="98" />
-            <toolbox:rectbound x="-5" y="97" width="24" height="1" />
-        </g>
-        <g class="BUE" id="BueKT">
-            <rect width="25" height="35" fill="white" />
-            <text font-size="30" stroke-width="0" x="12.5" y="28" font-family="siteplanfont" text-anchor="middle">
-                K
-            </text>
-            <toolbox:anchor id="mastAnkerTop" x="12.5" y="0" />
-            <toolbox:anchor id="mastAnkerBottom" x="12.5" y="35" />
-            <toolbox:rectbound x="0" y="0" width="25" height="35" />
-        </g>
-        <g class="BUE" id="BueAT">
-            <rect width="25" height="35" fill="white" />
-            <text font-size="30" stroke-width="0" x="12.5" y="28" font-family="siteplanfont" text-anchor="middle">
-                A
-            </text>
-            <text id="km" font-size="15" stroke-width="0" x="28" y="40" font-family="siteplanfont" />
-            <toolbox:anchor id="mastAnkerTop" x="12.5" y="0" />
-            <toolbox:anchor id="mastAnkerBottom" x="12.5" y="35" />
-            <toolbox:rectbound x="0" y="0" width="25" height="35" />
-        </g>
-        <g class="BUE" id="BueATWdh">
-            <rect width="25" height="35" fill="white" />
-            <text font-size="30" stroke-width="0" x="12.5" y="28" font-family="siteplanfont" text-anchor="middle">
-                A
-            </text>
-            <text id="km" font-size="15" stroke-width="0" x="28" y="40" font-family="siteplanfont" />
-            <toolbox:anchor id="mastAnkerTop" x="12.5" y="0" />
-            <toolbox:anchor id="mastAnkerBottom" x="12.5" y="35" />
-            <toolbox:rectbound x="0" y="0" width="25" height="35" />
-        </g>
-        <g class="BUE" id="BueATZusatz">
-            <rect width="25" height="35" fill="white" />
-            <text font-size="30" stroke-width="0" x="12.5" y="28" font-family="siteplanfont" text-anchor="middle">
-                B
-            </text>
-            <text font-size="13" stroke-width="0" x="30" y="-20" font-family="siteplanfont">
-                Bü
-            </text>
-            <text font-size="13" stroke-width="0" x="30" y="-30" font-family="siteplanfont">
-                Bü
-            </text>
-            <line x1="25" x2="34" y1="0" y2="-18" stroke-width="1.5" />
-            <toolbox:anchor id="mastAnkerTop" x="12.5" y="0" />
-            <toolbox:anchor id="mastAnkerBottom" x="12.5" y="35" />
-            <toolbox:rectbound x="-1.25" y="-1.25" width="27.5" height="37.5" />
-            <toolbox:rectbound x="27" y="-38" width="15.5" height="37.5" />
-            <toolbox:rectbound x="0" y="0" width="25" height="35" />
-            <toolbox:rectbound x="27.5" y="-38" width="17" height="35.5" />
-        </g>
-    </defs>
-    <g width="100" height="100" transform="translate(30,50)">
-        <use xlink:href="#BueATZusatz" />
-    </g>
-</svg>`
+    function normalized_direction (a: Coordinate, b: Coordinate): Coordinate | undefined {
+      const len = distance_coord(a,b)
+      if (len <= 0.000001) {
+        return undefined
+      }
 
-    // icon.setDisplacement([track.sections[0].segments[0].positions[0].x, track.sections[0].segments[0].positions[0].y])
+      return {
+        x: (b.x - a.x) / len,
+        y: (b.y - a.y) / len
+      }
+    }
 
-    const feature = createFeature(
-      FeatureType.TrackDirectionArrow,
-      {
-        guid: 'TestGuid',
-        data: track
-      },
-      geometry,
-      undefined // no label
-    )
+    for (const section of track.sections) {
+      for (const segment of section.segments) {
+        const segmentparts_length = []
+        const cumulative_length = [0.0]
+
+        for (let i = 1; i < segment.positions.length; i++) {
+          const length_this_part = distance_coord(segment.positions[i - 1],segment.positions[i])
+          // console.log(length_this_part)
+          segmentparts_length.push(length_this_part)
+          cumulative_length.push(cumulative_length.at(-1)! + length_this_part)
+        }
+
+        if (cumulative_length.at(-1) == undefined || cumulative_length.at(-1)! < 0.00001) {
+          // part too short to display!
+          continue
+        }
+
+        const half_length = cumulative_length.at(-1)! * 0.66 // place at 2/3rds
+        // len zero => last index = -1. In that case, 0 should be returned
+        // const last_pos_before_midle: number =
+        // Math.max(0,cumulative_length.findLastIndex(value => value < half_length)!)
+        const last_pos_before_midle: number = cumulative_length
+          .findLastIndex(value => value < half_length)!
+
+        const direction_vector = normalized_direction(
+          segment.positions[last_pos_before_midle],
+          segment.positions[last_pos_before_midle + 1]
+        )
+
+        if (direction_vector != undefined) {
+          const remaining_length = half_length - cumulative_length[last_pos_before_midle]
+          const middle = {
+            x: segment.positions[last_pos_before_midle].x + direction_vector.x * remaining_length,
+            y: segment.positions[last_pos_before_midle].y + direction_vector.y * remaining_length
+          }
+
+          // markers.push([middle, direction_vector])
+
+          const geometry = new OlPoint([
+            middle.x,
+            middle.y
+          ])
+
+          const angleRad = - Math.atan2(direction_vector.y, direction_vector.x)  // y, x
+          const angleDeg = angleRad * (180 / Math.PI)
+
+          const feature = createFeature(
+            FeatureType.TrackDirectionArrow,
+            {
+              guid: 'TestGuid',
+              data: angleRad
+            },
+            geometry,
+            undefined // no label
+          )
+          feature.setStyle(TrackDirectionFeature.TDF_FEATURE)
+
+          markers.push(feature)
+        } else {
+          // dir_vec = undefined <=> len of that part is zero.
+          // In that case: no top-dir can sensibly be shown!
+        }
+      }
+    }
 
     // this.createArrowBBox(feature, track, svg)
 
-    feature.setStyle(TrackDirectionFeature.TDF_FEATURE)
-
-    return feature
+    return markers
   }
 
   private createArrowBBox (feature: Feature<Geometry>, track: Track, svg: ISvgElement) {
