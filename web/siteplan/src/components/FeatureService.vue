a0e40eff9ec2b0645cba58825a2e8f80ebf6e517
TruongQuangSB <131350493+TruongQuangSB@users.noreply.github.com>
2025-10-30 14:22:38 +0100

    Update FeatureService.vue

--- web/siteplan/src/components/FeatureService.vue
+++ web/siteplan/src/components/FeatureService.vue
@@ -137,7 +137,7 @@ export default class FeatureService extends Vue {
     // Download the current model
     const modelType = store.state.planproModelType
     axios
-      .get<SiteplanModel>('/siteplanKR.json')
+      .get<SiteplanModel>(`/${modelType}.json`)
       .then(response => {
         this.modelLoaded(response.data)
         store.commit('setLoading', false)
