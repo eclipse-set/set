684584d97581dd7f278c0dee2b055654f6cea9fd
Marius Heine <m.heine@geprog.com>
2025-11-25 01:28:45 +0100

    cleanup unit test

--- java/bundles/org.eclipse.set.utils.test/src/org/eclipse/set/utils/ClothoidTest.java
+++ java/bundles/org.eclipse.set.utils.test/src/org/eclipse/set/utils/ClothoidTest.java
@@ -17,11 +17,11 @@ import org.eclipse.set.utils.math.Clothoid;
 import org.junit.jupiter.api.Test;
 
 /**
- * Tests for {@link Clothoid}. IMPROVE: actualy given't reliable test data (also
- * Clothoid coordinate). Therefore this test should be for the first disable
+ * Tests for {@link Clothoid}.
  * 
  * @author Stuecker
  */
+@SuppressWarnings("nls")
 public class ClothoidTest {
 	List<double[]> result;
 
@@ -40,22 +40,20 @@ public class ClothoidTest {
 	public void testReferenceCalculation() {
 		givenUnitClothoid();
 		givenGeometryOptions(11);
-		givenExpectedResult(0.0, 0.0, // 0.0
-				0.100000, 0.000167, // 0.1
-				0.199992, 0.001333, // 0.2
-				0.299939, 0.004499, // 0.3
-				0.399744, 0.010662, // 0.4
-				0.499219, 0.020810, // 0.5
-				0.598059, 0.035917, // 0.6
-				0.695810, 0.056922, // 0.7
-				0.791847, 0.084711, // 0.8
-				0.885349, 0.120084, // 0.9
-				// 0.970298, 0.197019 // 1.0
-				0.975288, 0.163714 // 1.0
+		givenExpectedResult(0.0, 0.0, // l=0.0
+				0.100000, 0.000167, // l=0.1
+				0.199992, 0.001333, // l=0.2
+				0.299939, 0.004499, // l=0.3
+				0.399744, 0.010662, // l=0.4
+				0.499219, 0.020810, // l=0.5
+				0.598059, 0.035917, // l=0.6
+				0.695810, 0.056922, // l=0.7
+				0.791847, 0.084711, // l=0.8
+				0.885349, 0.120084, // l=0.9
+				0.975288, 0.163714 // l=1.0
 		);
 		whenCalculatingUnitClothoid();
-		assertPositionEquality(expected.get(10), result.get(10), 10);
-		// thenResultMatchesReference();
+		thenResultMatchesReference();
 	}
 
 	/**
@@ -67,35 +65,21 @@ public class ClothoidTest {
 	public void testReferenceEggCalculation() {
 		givenEggClothoid();
 		givenGeometryOptions(11);
-		givenExpectedResult(0.0, 0.0, // 0.0
-				0.100000, 0.000167, // 0.1
-				0.199992, 0.001333, // 0.2
-				0.299939, 0.004499, // 0.3
-				0.399744, 0.010662, // 0.4
-				0.499219, 0.020810, // 0.5
-				0.598059, 0.035917, // 0.6
-				0.695810, 0.056922, // 0.7
-				0.791847, 0.084711, // 0.8
-				0.885349, 0.120084, // 0.9
-				// 0.970298, 0.197019 // 1.0
-				0.975288, 0.163714 // 1.0
+		givenExpectedResult(0.0, 0.0, // l=0.0
+				0.100000, 0.000167, // l=0.1
+				0.199992, 0.001333, // l=0.2
+				0.299939, 0.004499, // l=0.3
+				0.399744, 0.010662, // l=0.4
+				0.499219, 0.020810, // l=0.5
+				0.598059, 0.035917, // l=0.6
+				0.695810, 0.056922, // l=0.7
+				0.791847, 0.084711, // l=0.8
+				0.885349, 0.120084, // l=0.9
+				0.975288, 0.163714 // l=1.0
 		);
 		whenCalculatingUnitClothoid();
-		assertPositionEquality(expected.get(10), result.get(10), 10);
-		// thenResultMatchesReference();
+		thenResultMatchesReference();
 	}
-
-	/**
-	 * Tests {@link Clothoid} using reference values for the unit clothoid
-	 * (clothoid with A^2 = 1)
-	 */
-	// @Test
-	// public void testSegmentCountAwareness() {
-	// givenUnitClothoid();
-	// givenGeometryOptions(20);
-	// whenCalculatingUnitClothoid();
-	// thenResultMatchesReference();
-	// }
 
 	private static void assertPositionEquality(final double[] expected,
 			final double[] actual, final int index) {
@@ -106,7 +90,7 @@ public class ClothoidTest {
 	}
 
 	private void givenUnitClothoid() {
-		testee = new Clothoid(1, 1, 5);
+		testee = new Clothoid(1, 1, 10);
 	}
 
 	private void givenEggClothoid() {
