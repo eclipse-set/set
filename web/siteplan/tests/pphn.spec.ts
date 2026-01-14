b4d076beb6d00202a611ab068482c9183a404a8a
Marius Heine <m.heine@geprog.com>
2026-01-14 08:50:12 +0100

    more stable initial loading of app

--- web/siteplan/tests/pphn.spec.ts
+++ web/siteplan/tests/pphn.spec.ts
@@ -1,22 +1,24 @@
-import { expect, test } from '@playwright/test'
+import { expect, Page, test } from '@playwright/test'
 import pphn from './data/PPHN_1.10.0.3_01-02_Ibn-Z._-_2._AeM_2022-05-17_13-44_tg3.json' with { type: 'json' }
 
-test('initial loading', async ({ page }) => {
+async function loadSiteplan (page: Page) {
   await page.route('*/**/siteplan.json', async route => {
     await route.fulfill({ json: pphn })
   })
 
   await page.goto('/')
+  await expect(page.locator('.loading')).toBeVisible()
+  await expect(page.locator('.loading')).not.toBeVisible()
+}
+
+test('initial loading', async ({ page }) => {
+  await loadSiteplan(page)
 
   await expect(page).toHaveScreenshot('pphn-initial-view.png')
 })
 
 test('total view displaying', async ({ page }) => {
-  await page.route('*/**/siteplan.json', async route => {
-    await route.fulfill({ json: pphn })
-  })
-
-  await page.goto('/')
+  await loadSiteplan(page)
 
   await page.getByRole('button', { name: '▣' }).click()
   await expect(page).toHaveScreenshot('pphn-total-view.png')
