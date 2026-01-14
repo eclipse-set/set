e1a6fb4f1ae0df4e18dd5c49b3704000a01dfaa8
Marius Heine <m.heine@geprog.com>
2026-01-14 08:39:19 +0100

    add total view test

--- web/siteplan/tests/pphn.spec.ts
+++ web/siteplan/tests/pphn.spec.ts
@@ -1,12 +1,27 @@
 import { expect, test } from '@playwright/test'
 import pphn from './data/PPHN_1.10.0.3_01-02_Ibn-Z._-_2._AeM_2022-05-17_13-44_tg3.json' with { type: 'json' }
 
-test('load pphn', async ({ page }) => {
+test('initial loading', async ({ page }) => {
   await page.route('*/**/siteplan.json', async route => {
     await route.fulfill({ json: pphn })
   })
 
   await page.goto('/')
 
-  await expect(page).toHaveScreenshot('pphn-default.png', { timeout: 10_000 })
+  await expect(page).toHaveScreenshot('pphn-initial-view.png')
+})
+
+test('total view displaying', async ({ page }) => {
+  await page.route('*/**/siteplan.json', async route => {
+    await route.fulfill({ json: pphn })
+  })
+
+  await page.goto('/')
+
+  await page.getByRole('button', { name: '▣' }).click()
+  await expect(page).toHaveScreenshot('pphn-total-view.png')
+
+  await page.getByRole('button', { name: '⦻' }).click()
+  await page.mouse.move(0, 0) // move mouse to upper left corner again to get remove hover effect from center button
+  await expect(page).toHaveScreenshot('pphn-initial-view.png')
 })
