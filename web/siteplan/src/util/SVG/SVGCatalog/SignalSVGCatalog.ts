71c0a586ce01f3cf1b62108c8e4dc65a4e797d14
Jan-Erik Voigt <voigt.jan-erik@scheidt-bachmann-st.de>
2026-01-15 13:53:18 +0100

    getSignalScreen only returns screens which should actually be drawn

--- web/siteplan/src/util/SVG/SVGCatalog/SignalSVGCatalog.ts
+++ web/siteplan/src/util/SVG/SVGCatalog/SignalSVGCatalog.ts
@@ -11,6 +11,9 @@ import { Signal } from '@/model/Signal'
 import { ISvgElement } from '@/model/SvgElement'
 import AbstractSVGCatalog from './AbstractSVGCatalog'
 
+const notDrawnScreens: string[] = ['Zs1','Zs8']
+const onlyDrawnAlleinstehend: string[] = ['Ne14', 'Zs2', 'Zs3', 'Zs3v', 'Zs6']
+
 export default abstract class SignalSVGCatalog extends AbstractSVGCatalog {
   /**
      * Find Screen of the Signal
@@ -18,7 +21,23 @@ export default abstract class SignalSVGCatalog extends AbstractSVGCatalog {
      * @returns first screen of this signal
      */
   public getSignalScreen (signal: Signal): ISvgElement | null {
-    for (const screen of signal.screen) {
+    // console.log('screens:',signal.screen)
+    if (signal.guid === '167D036D-6B63-4694-BD66-3141EAD98743') {
+      console.log('7707')
+    }
+
+    let drawnSignals = signal.screen.filter(s => !notDrawnScreens.includes(s.screen))
+
+    if (drawnSignals.length > 1) {
+      // don't draw alleinstehende screens
+      drawnSignals = drawnSignals.filter(s => !onlyDrawnAlleinstehend.includes(s.screen))
+    }
+
+    for (const screen of drawnSignals) {
+      if (screen?.screen === 'Ne14') {
+        console.log('...')
+      }
+
       const result = this.getSVGFromCatalog(screen.screen)
       if (result !== null) {
         return result
@@ -29,3 +48,4 @@ export default abstract class SignalSVGCatalog extends AbstractSVGCatalog {
 
   public abstract catalogName (): string
 }
+
