4cfcfe2b271f527111f911b16502ba32090274e2
Jan-Erik Voigt <voigt.jan-erik@scheidt-bachmann-st.de>
2026-01-15 14:55:34 +0100

    remove console logs

--- web/siteplan/src/util/SVG/SVGCatalog/SignalSVGCatalog.ts
+++ web/siteplan/src/util/SVG/SVGCatalog/SignalSVGCatalog.ts
@@ -21,11 +21,6 @@ export default abstract class SignalSVGCatalog extends AbstractSVGCatalog {
      * @returns first screen of this signal
      */
   public getSignalScreen (signal: Signal): ISvgElement | null {
-    // console.log('screens:',signal.screen)
-    if (signal.guid === '167D036D-6B63-4694-BD66-3141EAD98743') {
-      console.log('7707')
-    }
-
     let drawnSignals = signal.screen.filter(s => !notDrawnScreens.includes(s.screen))
 
     if (drawnSignals.length > 1) {
@@ -34,10 +29,6 @@ export default abstract class SignalSVGCatalog extends AbstractSVGCatalog {
     }
 
     for (const screen of drawnSignals) {
-      if (screen?.screen === 'Ne14') {
-        console.log('...')
-      }
-
       const result = this.getSVGFromCatalog(screen.screen)
       if (result !== null) {
         return result
@@ -48,4 +39,3 @@ export default abstract class SignalSVGCatalog extends AbstractSVGCatalog {
 
   public abstract catalogName (): string
 }
-
