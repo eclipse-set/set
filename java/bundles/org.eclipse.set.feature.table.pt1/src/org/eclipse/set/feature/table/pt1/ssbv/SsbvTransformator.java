57e5fe513beb6816710ccf3f242fe58bcb93ab2d
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2026-01-28 09:38:49 +0100

    Designation

--- java/bundles/org.eclipse.set.feature.table.pt1/src/org/eclipse/set/feature/table/pt1/ssbv/SsbvTransformator.java
+++ java/bundles/org.eclipse.set.feature.table.pt1/src/org/eclipse/set/feature/table/pt1/ssbv/SsbvTransformator.java
@@ -10,16 +10,35 @@
  */
 package org.eclipse.set.feature.table.pt1.ssbv;
 
+import static org.eclipse.set.ppmodel.extensions.EObjectExtensions.getNullableObject;
+
+import java.util.List;
 import java.util.Set;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.set.core.services.enumtranslation.EnumTranslationService;
 import org.eclipse.set.feature.table.pt1.AbstractPlanPro2TableModelTransformator;
+import org.eclipse.set.model.planpro.Ansteuerung_Element.Aussenelementansteuerung;
+import org.eclipse.set.model.planpro.Ansteuerung_Element.ESTW_Zentraleinheit;
 import org.eclipse.set.model.planpro.Ansteuerung_Element.Stell_Bereich;
+import org.eclipse.set.model.planpro.BasisTypen.ID_Bearbeitungsvermerk_TypeClass;
+import org.eclipse.set.model.planpro.Basisobjekte.Bearbeitungsvermerk;
+import org.eclipse.set.model.planpro.Bedienung.Bedien_Einrichtung_Oertlich;
+import org.eclipse.set.model.planpro.Ortung.FMA_Anlage;
+import org.eclipse.set.model.planpro.Ortung.FMA_Komponente;
+import org.eclipse.set.model.planpro.Ortung.Zugeinwirkung;
+import org.eclipse.set.model.planpro.Signale.Signal;
 import org.eclipse.set.model.tablemodel.ColumnDescriptor;
 import org.eclipse.set.model.tablemodel.Table;
+import org.eclipse.set.model.tablemodel.TableRow;
+import org.eclipse.set.ppmodel.extensions.AussenelementansteuerungExtensions;
+import org.eclipse.set.ppmodel.extensions.EObjectExtensions;
+import org.eclipse.set.ppmodel.extensions.UrObjectExtensions;
 import org.eclipse.set.ppmodel.extensions.container.MultiContainer_AttributeGroup;
 import org.eclipse.set.utils.table.TMFactory;
 import org.osgi.service.event.EventAdmin;
+
+import com.google.common.collect.Streams;
 
 /**
  * Table transformation for a Bearbeitungsvermerke tabelle
@@ -44,10 +63,74 @@ public class SsbvTransformator extends AbstractPlanPro2TableModelTransformator {
 
 	@Override
 	public Table transformTableContent(
-			final MultiContainer_AttributeGroup model, final TMFactory factory,
-			final Stell_Bereich controlArea) {
-		// TODO Auto-generated method stub
-		return null;
+			final MultiContainer_AttributeGroup container,
+			final TMFactory factory, final Stell_Bereich controlArea) {
+		final List<ID_Bearbeitungsvermerk_TypeClass> idReferences = Streams
+				.stream(container.getAllContents())
+				.filter(ID_Bearbeitungsvermerk_TypeClass.class::isInstance)
+				.map(ID_Bearbeitungsvermerk_TypeClass.class::cast)
+				.toList();
+
+		for (final Bearbeitungsvermerk bv : container
+				.getBearbeitungsvermerk()) {
+			if (Thread.currentThread().isInterrupted()) {
+				return null;
+			}
+			final TableRow row = factory.newTableRow(bv);
+
+			// A: Bearbeitungsvermerke inhalt
+			fill(row, getColumn(cols, SsbvColumns.Text_Content), bv,
+					note -> EObjectExtensions
+							.getNullableObject(note,
+									e -> e.getBearbeitungsvermerkAllg()
+											.getKurztext()
+											.getWert())
+							.orElse("")); //$NON-NLS-1$
+
+			// B: Referenziert von Objects
+			fillIterable(row, getColumn(cols, SsbvColumns.Reference_Object), bv,
+					note -> idReferences.stream()
+							.filter(ref -> ref.getValue().equals(note))
+							.map(EObject::eContainer)
+							.map(SsbvTransformator::getReferenceObjDesignation)
+							.toList(),
+					MIXED_STRING_COMPARATOR);
+		}
+		return factory.getTable();
 	}
 
+	@SuppressWarnings("nls")
+	private static String getReferenceObjDesignation(final EObject refObj) {
+		final String typeName = UrObjectExtensions.getTypeName(refObj);
+		final String objDesignation = switch (refObj) {
+			case final Aussenelementansteuerung aussenelement -> AussenelementansteuerungExtensions
+					.getElementBezeichnung(aussenelement);
+			case final Bedien_Einrichtung_Oertlich beo -> getNullableObject(beo,
+					e -> e.getBezeichnung()
+							.getBedienEinrichtOertlBez()
+							.getWert()).orElse("");
+			case final ESTW_Zentraleinheit estwZentral -> AussenelementansteuerungExtensions
+					.getElementBezeichnung(estwZentral);
+			case final FMA_Anlage fmaAnlage -> getNullableObject(fmaAnlage,
+					fma -> fma.getFMAAnlageKaskade()
+							.getFMAKaskadeBezeichnung()
+							.getWert()).orElse("");
+			case final FMA_Komponente fmaKomponent -> getNullableObject(
+					fmaKomponent,
+					fma -> fma.getBezeichnung()
+							.getBezeichnungTabelle()
+							.getWert()).orElse("");
+			case final Signal signal -> getNullableObject(signal,
+					s -> s.getBezeichnung().getBezeichnungTabelle().getWert())
+							.orElse("");
+			case final Zugeinwirkung ein -> getNullableObject(ein,
+					e -> e.getBezeichnung().getBezeichnungTabelle().getWert())
+							.orElse("");
+			default -> "";
+		};
+		if (objDesignation != null && !objDesignation.isEmpty()) {
+			return typeName + " " + objDesignation;
+		}
+		return typeName;
+	}
 }
