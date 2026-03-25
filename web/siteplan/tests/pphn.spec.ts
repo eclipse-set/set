0409b74993a6dc443c8646ce4fdec81fd1228041
Marius Heine <m.heine@geprog.com>
2026-03-25 01:28:29 +0100

    test case for non development mode

--- web/siteplan/tests/pphn.spec.ts
+++ web/siteplan/tests/pphn.spec.ts
@@ -1,4 +1,5 @@
 import { expect, Page, test } from '@playwright/test'
+import configuration from '../public/configuration.json' with { type: 'json' }
 import pphn from './data/PPHN_1.10.0.3_01-02_Ibn-Z._-_2._AeM_2022-05-17_13-44_tg3.json' with { type: 'json' }
 
 async function loadSiteplan (page: Page) {
@@ -30,4 +31,14 @@ test('total view displaying', async ({ page }) => {
   await page.getByRole('button', { name: '⦻' }).click()
   await page.mouse.move(0, 0) // move mouse to upper left corner again to get remove hover effect from center button
   await expect(page).toHaveScreenshot('pphn-initial-view.png', screenshotOptions(page))
+})
+
+test('no development mode', async ({ page }) => {
+  await page.route('*/**/configuration.json', async route => {
+    await route.fulfill({ json: { ...configuration, developmentMode: false } })
+  })
+
+  await loadSiteplan(page)
+
+  await expect(page).toHaveScreenshot('pphn-no-development-mode.png', screenshotOptions(page))
 })
