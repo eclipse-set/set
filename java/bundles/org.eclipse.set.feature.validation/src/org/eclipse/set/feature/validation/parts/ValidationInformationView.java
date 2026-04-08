5631cd97cdf5f527b34f9d284855774795f979e0
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2026-04-08 14:44:59 +0200

    Initial validation view

--- java/bundles/org.eclipse.set.feature.validation/src/org/eclipse/set/feature/validation/parts/ValidationInformationView.java
+++ java/bundles/org.eclipse.set.feature.validation/src/org/eclipse/set/feature/validation/parts/ValidationInformationView.java
@@ -7,12 +7,41 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  * 
- */ 
+ */
 package org.eclipse.set.feature.validation.parts;
+
+import org.eclipse.set.model.validationreport.ValidationReport;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.ExpandBar;
+import org.eclipse.swt.widgets.ExpandItem;
 
 /**
  * 
  */
 public class ValidationInformationView {
+
+	private final Composite parent;
+	private final ValidationReport validationReport;
+	private static final String MODEL_INFOMATIONEN = "Modellinformationen";
+
+	public ValidationInformationView(final Composite parent,
+			final ValidationReport validationReport) {
+		this.parent = parent;
+		this.validationReport = validationReport;
+		createView();
+	}
+
+	private void createView() {
+		final ExpandBar expandBar = new ExpandBar(parent, SWT.None);
+		expandBar.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+	}
+
+	private void createModelInformation(final ExpandBar expandBar) {
+		final ExpandItem expandItem = new ExpandItem(expandBar, SWT.None);
+		expandItem.setText(MODEL_INFOMATIONEN);
+
+	}
 
 }
