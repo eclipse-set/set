e0cff6d479788bf236e21d71c670344bdc30fdff
Linus Brombacher <Brombacher.Linus@scheidt-bachmann-st.de>
2026-06-09 08:01:29 +0200

    iterate over multiple search results in search test

--- web/siteplan/tests/search.spec.ts
+++ web/siteplan/tests/search.spec.ts
@@ -6,9 +6,23 @@ test('find search results', async ({ page }) => {
   const searchBar = page.getByRole('textbox')
   await searchBar.click()
   await searchBar.fill('C4C2FC46-CA76-46D0-87DD-FB158441603C')
-  await searchBar.press('Enter')
 
-  await expect(page).toHaveScreenshot('search-result.png')
   await searchBar.press('Enter')
-  await expect(page).toHaveScreenshot('search-result-second.png')
+  await expect(page).toHaveScreenshot('search-result-one.png')
+
+  await searchBar.press('Enter')
+  await expect(page).toHaveScreenshot('search-result-two.png')
+
+  await searchBar.press('Enter')
+  await expect(page).toHaveScreenshot('search-result-three.png')
+
+  await searchBar.press('Enter')
+  await expect(page).toHaveScreenshot('search-result-four.png')
+
+  await searchBar.press('Enter')
+  await expect(page).toHaveScreenshot('search-result-one-again.png')
+  // await expect(page).toHaveScreenshot('search-result-one.png')
+
+  await searchBar.press('Enter')
+  await expect(page).toHaveScreenshot('search-result-two.png')
 })
