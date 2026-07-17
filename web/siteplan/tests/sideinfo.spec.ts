973c22e955973b8035c8d6972133b3401ace20e1
Marius Heine <m.heine@geprog.com>
2026-07-17 13:13:00 +0200

    add screenshots for sideinfo containers

--- web/siteplan/tests/sideinfo.spec.ts
+++ web/siteplan/tests/sideinfo.spec.ts
@@ -1,5 +1,9 @@
-import { expect, test } from '@playwright/test'
+import { expect, Page, test } from '@playwright/test'
 import { loadSiteplan, setDevelopmentMode } from './utils'
+
+async function expectScreenshot (page: Page, name: string) {
+  await expect(page.locator('#side-info-container')).toHaveScreenshot(`${name}-side-info.png`, { })
+}
 
 test('show map selection', async ({ page }) => {
   await setDevelopmentMode(page, true)
@@ -8,6 +12,7 @@ test('show map selection', async ({ page }) => {
   await page.getByTitle('Kartenquelle').click()
 
   await expect(page.locator('.map-container')).toBeVisible()
+  expectScreenshot(page, 'map-container')
 })
 
 test('show layer control', async ({ page }) => {
@@ -17,6 +22,7 @@ test('show layer control', async ({ page }) => {
   await page.getByTitle('Ebenen verwalten').click()
 
   await expect(page.getByRole('heading', { name: 'Hinweis:' })).toBeVisible()
+  expectScreenshot(page, 'layer-control')
 })
 
 test('show model summary control', async ({ page }) => {
@@ -27,6 +33,7 @@ test('show model summary control', async ({ page }) => {
 
   await expect(page.getByText('[Hinzugefügt, Unverändert, Entfernt, Geändert')).toBeVisible()
   await expect(page.getByText('Bahnsteige:')).toBeVisible()
+  expectScreenshot(page, 'model-summary')
 })
 
 test('show settings control', async ({ page }) => {
@@ -36,4 +43,5 @@ test('show settings control', async ({ page }) => {
   await page.getByRole('button', { name: 'settings' }).click()
 
   await expect(page.getByText('Einstellungen')).toBeVisible()
+  expectScreenshot(page, 'settings')
 })
