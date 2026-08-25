db4d0c7d6212b0df11738e39ca683cc86f5e79c3
Quang Truong <truong.quang@scheidt-bachmann-st.de>
2026-08-25 16:39:20 +0200

    Remove comment code

--- java/bundles/org.eclipse.set.feature.validation/src/org/eclipse/set/feature/validation/parts/ValidationPart.java
+++ java/bundles/org.eclipse.set.feature.validation/src/org/eclipse/set/feature/validation/parts/ValidationPart.java
@@ -55,7 +55,6 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import jakarta.inject.Inject;
 
@@ -108,13 +107,6 @@ public class ValidationPart extends BasePart {
 
 	private Listener resizeListener;
 	private Composite resizeListenerObject;
-
-	@PostConstruct
-	void postConstruct() {
-		// getBroker().subscribe(Events.CLOSE_SESSION, event -> {
-		// modelService.put(INJECT_VIEW_VALIDATION_NATTABLE, null);
-		// });
-	}
 
 	/**
 	 * Create the part.
